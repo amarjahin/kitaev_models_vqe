02ef93e8368c6f22d9f066edc44cb9541089da04
Ammar Jahin <amar.jahin@gmail.com>
2021-06-24 10:26:24 -0400

    remove projection from cost

--- cost.py
+++ cost.py
@@ -1,21 +1,10 @@
 from numpy import conjugate
 from qiskit import QuantumCircuit, transpile
 
-def energy_ev(hamiltonian,simulator, qc_c, params, phys_check=False, projector=None):
+def energy_ev(hamiltonian,simulator, qc_c, params):
     
     qc_c = qc_c.bind_parameters(params)
     result = simulator.run(qc_c).result()
     state_vec = result.get_statevector()
-    energy_ev = (conjugate(state_vec.T) @ hamiltonian @ state_vec).real
-    if not phys_check: 
-        return energy_ev
-    else: 
-        # this else is still highly experimental and can be just ignored at this point 
-        phys_prob = conjugate(state_vec.T) @ projector @ state_vec 
-        if phys_prob < 1e-3: 
-            return 1e3 
-            # return energy_ev
-
-        else: 
-            return energy_ev
-
+    energy_ev = (conjugate(state_vec.T) @ hamiltonian @ state_vec).real 
+    return energy_ev
