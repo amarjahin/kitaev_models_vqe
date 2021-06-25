d7903f95edf685bb703855001aedc72fff1b1b13
Ammar Jahin <amar.jahin@gmail.com>
2021-06-25 16:47:16 -0400

    change the cost to account for unphysical states

--- cost.py
+++ cost.py
@@ -1,10 +1,12 @@
 from numpy import conjugate
 from qiskit import QuantumCircuit, transpile
 
-def energy_ev(hamiltonian,simulator, qc_c, params):
+def energy_ev(hamiltonian,simulator, qc_c, params, projector):
     
     qc_c = qc_c.bind_parameters(params)
     result = simulator.run(qc_c).result()
     state_vec = result.get_statevector()
-    energy_ev = (conjugate(state_vec.T) @ hamiltonian @ state_vec).real 
-    return energy_ev
+    phys_prop = conjugate(state_vec.T) @ projector @ state_vec
+    # energy_ev = (conjugate(state_vec.T) @ hamiltonian @ state_vec).real 
+    phys_energy_ev = (conjugate(state_vec.T) @ projector @ hamiltonian @ state_vec).real/(phys_prop)
+    return phys_energy_ev
