89eb8a0b3f053f81fdfd5b26ded07476337aa5e4
Ammar Jahin <amar.jahin@gmail.com>
2021-07-13 10:58:31 -0400

    update for 16 qubits

--- time_diagnosis/qasm_v_statesim.py
+++ time_diagnosis/qasm_v_statesim.py
@@ -7,34 +7,41 @@ from qiskit.opflow import CircuitStateFn, PauliExpectation, CircuitSampler, Stat
 from qiskit.opflow import PauliOp, PauliSumOp
 from qiskit.quantum_info import Pauli
 
-qubit_op = PauliOp(Pauli('IIZZIZZZ')) + PauliOp(Pauli('IIXXXZZX')) 
+qubit_op = PauliOp(Pauli('IIZZIZZZIIZZIZZZ')) + PauliOp(Pauli('IIXXXZZXIIZZIZZZ')) 
 start = time.time()
-qc = QuantumCircuit(8)
+qc = QuantumCircuit(16)
 for i in range(4):
-    for j in range(7): 
+    for j in range(15): 
         qc.cx(j, j+1)
     qc.rz(pi/2, 7)
-    for j in range(7)[::-1]: 
+    for j in range(15)[::-1]: 
         qc.cx(j, j+1)
-    qc.h([*range(8)])
+    qc.h([*range(16)])
 
 qc = transpile(qc)  
 
 print(f'time took to build the circuit: {time.time() - start}')
 start = time.time()
 simulator = StatevectorSimulator()
-for _ in range(200):
+for _ in range(10):
     result = simulator.run(qc).result()
+    result = simulator.run(qc).result()
+
     psi = result.get_statevector()
-    qubit_mat = qubit_op.to_matrix()
+    # qubit_mat = qubit_op.to_spmatrix()
+    qubit_mat = qubit_op.to_spmatrix()
+    qubit_mat = qubit_op.to_spmatrix()
+
     qubit_op_ev = conjugate(psi.T) @ qubit_mat @ psi
+    qubit_op_ev = conjugate(psi.T) @ qubit_mat @ psi
+
 t1 = time.time() - start
 print(f'time to calculate expectation value using StatevectorSimulator: {round(t1, 4)}s')
 
 start = time.time()
 simulator = QasmSimulator()
-QI = QuantumInstance(simulator, shots=1000)
-for _ in range(200):
+QI = QuantumInstance(simulator, shots=2000)
+for _ in range(10):
     psi = CircuitStateFn(qc)
     ms = StateFn(qubit_op, is_measurement=True) @ psi
     pe = PauliExpectation().convert(ms)
--- time_diagnosis/time_diagnosis.py
+++ time_diagnosis/time_diagnosis.py
@@ -1,41 +1,56 @@
 import time 
-from numpy import pi
+from numpy import pi, conjugate
 from numpy.random import random, rand
 from qiskit import QuantumCircuit, transpile
 from qiskit.circuit import ParameterVector
-from qiskit.providers.aer import StatevectorSimulator, UnitarySimulator, QasmSimulator
-
+from qiskit.providers.aer import StatevectorSimulator
+from qiskit.opflow import PauliOp, PauliSumOp
+from qiskit.quantum_info import Pauli
+h = PauliOp(Pauli('IIZZIZZZIIZZIZZZ')) + PauliOp(Pauli('IIXXXZZXIIZZIZZZ')) 
+h_mat = h.to_spmatrix()
 start = time.time()
-num_params = 86
-# number of terms != number of paramaters because I group some of the terms
-# under the same parameter 
-num_terms= 2*56 + 8*30 
+num_params = 240
+num_terms= 2*num_params
 params = ParameterVector('a', length = num_params)
-qc = QuantumCircuit(8)
+qc = QuantumCircuit(16)
 # This circuit is not the ansatz, but it looks like it. 
 # this construction of the circuit is worst that what it actually is in 
-# terms of the number of cx gates. I expect the actual 
+# terms of the number of cx gates.
 for i in range(num_terms):
-    for j in range(7): 
+    rz_pos = (i//2) % 14 + 1
+    for j in range(rz_pos): 
         qc.cx(j, j+1)
-    if i < 86:
-        qc.rz(params[i], 7)
-    else:
-        qc.rz(pi/4, 7)
-    for j in range(7)[::-1]: 
+    qc.rz(params[i//2], rz_pos)
+    for j in range(rz_pos)[::-1]: 
         qc.cx(j, j+1)
-    qc.h([*range(8)])
-
+    qc.h([*range(rz_pos)])
 qc = transpile(qc)  
 end = time.time()
-print(f'time took to build the circuit: {end - start}')
-nfev = 4279 # this is the number of times the optimizer calls the cost function. 
-# Just make the calling to the cost function as many times as the optimizer.
-for i in range(nfev): 
+print(f'time took to build the circuit: {round(end - start,3)}')
+nfev = 12000 # this is the number of times the optimizer calls the cost function. 
+# Just make the calling to the cost function roughly the same times as the optimizer.
+simulator = StatevectorSimulator()
+bind_params_time = 0 
+run_circuit_time = 0 
+expectation_time = 0 
+for i in range(100): 
+    btime = time.time()
     params = pi*random(num_params)
     qc_c = qc.bind_parameters(params)
-    simulator = StatevectorSimulator()
-    result = simulator.run(qc_c).result()
+    bind_params_time = bind_params_time + (time.time()-btime)
 
-    if i % 100 == 0: 
-        print(f'time took to call the function {i+1} times: {time.time() - end}')
+    rtime = time.time()
+    result = simulator.run(qc_c).result()
+    run_circuit_time = run_circuit_time + (time.time()-rtime)
+
+    etime = time.time()
+    psi = result.get_statevector()
+    e_ev = conjugate(psi.T) @ h_mat @ psi 
+    expectation_time = expectation_time + (time.time()-etime)
+
+full_time = time.time() - end            
+print(f'time took to call the function {100} times: {round(full_time,3)}s')
+print(f'of which time binding parameters: {round(bind_params_time,3)} ({round(bind_params_time/full_time * 100,3)}%)')
+print(f'of which time running circuit: {round(run_circuit_time,3)} ({round(run_circuit_time/full_time * 100,3)}%)')
+print(f'of which time calculating <H>: {round(expectation_time,3)} ({round(expectation_time /full_time * 100, 3)}%)')
+print(f'extrapolating to {10000} calls, it should take {round(full_time * (12000/100) / (60),3)}m')
