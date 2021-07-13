c51980072934f54d462b605b25183057f55c2bec
Ammar Jahin <amar.jahin@gmail.com>
2021-07-13 14:59:12 -0400

    include both projected and unprokected energy

--- cost.py
+++ cost.py
@@ -2,27 +2,22 @@ import os
 os.environ['MPMATH_NOSAGE'] = 'true'
 
 from numpy import conjugate
+from numpy.linalg import norm
 from qiskit import QuantumCircuit, transpile
 from qiskit.providers.aer import QasmSimulator, StatevectorSimulator
 from qiskit.opflow import CircuitStateFn, PauliExpectation, CircuitSampler, StateFn
 from qiskit.utils import QuantumInstance
 
 
-def phys_energy_ev(hamiltonian,simulator, qc_c, params, projector, return_projection=False):
+def phys_energy_ev(hamiltonian,simulator, qc_c, params, projector):
     qc_c = qc_c.bind_parameters(params)
     if isinstance(simulator, StatevectorSimulator):
-        hamiltonian = hamiltonian.to_matrix()
-        projector = projector.to_matrix()
         result = simulator.run(qc_c).result()
         state_vec = result.get_statevector()
-        phys_prop = conjugate(state_vec.T) @ projector @ state_vec
-        # energy_ev = (conjugate(state_vec.T) @ hamiltonian @ state_vec).real 
-        phys_energy_ev = (conjugate(state_vec.T) @ projector @ hamiltonian @ state_vec).real/(phys_prop)
-        if not return_projection:
-            return phys_energy_ev
-        else: 
-            return phys_energy_ev, phys_prop
-
+        phys_state_vec = projector @ state_vec
+        phys_state_vec = phys_state_vec/norm(phys_state_vec)
+        phys_energy_ev = (conjugate(phys_state_vec.T) @ hamiltonian @ phys_state_vec).real
+        return phys_energy_ev
     elif isinstance(simulator, QasmSimulator): 
         psi = CircuitStateFn(qc_c)
         QI = QuantumInstance(simulator, shots=10000)
@@ -32,10 +27,17 @@ def phys_energy_ev(hamiltonian,simulator, qc_c, params, projector, return_projec
         p_ev = PauliExpectation().convert(ms_p)
         ph_samp = CircuitSampler(QI).convert(ph_ev).eval().real
         p_samp = CircuitSampler(QI).convert(p_ev).eval().real
-        if not return_projection:
-            return ph_samp/p_samp
-        else: 
-            return ph_samp/p_samp, p_samp
-
+        return ph_samp/p_samp
     else: 
         raise ValueError('simulator can only be QasmSimulator or StatevectorSimulator')
+
+
+def energy_ev(hamiltonian,simulator, qc_c, params):
+    qc_c = qc_c.bind_parameters(params)
+    if isinstance(simulator, StatevectorSimulator):
+        result = simulator.run(qc_c).result()
+        state_vec = result.get_statevector()
+        energy_ev = (conjugate(state_vec.T) @ hamiltonian @ state_vec).real 
+        return  energy_ev
+    else: 
+        raise ValueError('simulator can only be StatevectorSimulator')
